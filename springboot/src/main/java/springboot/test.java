e321ac78e2ae67a00b9608ccc95f854c90f889bb
Asus <Asus@Asus-PC>
2020-03-04 12:35:50 +0530

    hello check my code

---
+++ springboot/src/main/java/springboot/test.java
@@ -0,0 +1,15 @@
+package springboot;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubS
+		
+		test2 tt=new test2();
+		tt.setId(101);
+		tt.setName("ali");
+		System.out.println(tt.getId()+""+"hello"+tt.getName());
+		
+	}
+
+}
